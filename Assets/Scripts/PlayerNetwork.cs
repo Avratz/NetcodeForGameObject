4869e5a099b7caeb372ecdd8c2bc927a3b192808
Avratz <signos97@gmail.com>
2023-02-22 18:09:40 -0300

    Network Variables, Server RPC, Client RPC

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,15 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
 
+
+  private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+    new MyCustomData
+    {
+      _int = 0,
+      _bool = false,
+      message = "Hello World"
+    },
+    NetworkVariableReadPermission.Everyone,
+    NetworkVariableWritePermission.Owner
+  );
+
+  private struct MyCustomData : INetworkSerializable
+  {
+    public int _int;
+    public bool _bool;
+    public FixedString128Bytes message;
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+      serializer.SerializeValue(ref _int);
+      serializer.SerializeValue(ref _bool);
+      serializer.SerializeValue(ref message);
+    }
+  }
+
+  public override void OnNetworkSpawn()
+  {
+    randomNumber.OnValueChanged += (MyCustomData oldValue, MyCustomData newValue) =>
+    {
+      Debug.Log(OwnerClientId + "; randomNumber: " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+    };
+  }
+
   private void Update()
   {
 
     if (!IsOwner) return;
+
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      TestServerRpc(new ServerRpcParams());
+      TestClientRpc(new ClientRpcParams
+      {
+        Send = new ClientRpcSendParams
+        {
+          TargetClientIds = new List<ulong> { 0 }
+        }
+      });
+      randomNumber.Value = new MyCustomData
+      {
+        _int = Random.Range(0, 100),
+        _bool = Random.Range(0, 2) == 0,
+        message = "Hello World 2"
+      };
+    }
+
 
     Vector3 moveDir = new Vector3(0, 0, 0);
 
@@ -32,5 +86,17 @@ public class PlayerNetwork : NetworkBehaviour
 
     float moveSpeed = 5f;
     transform.position += moveDir * Time.deltaTime * moveSpeed;
+  }
+
+  [ServerRpc]
+  private void TestServerRpc(ServerRpcParams serverRpcParams)
+  {
+    Debug.Log(OwnerClientId + " :: TestServerRpc :: " + serverRpcParams.Receive.SenderClientId);
+  }
+
+  [ClientRpc]
+  private void TestClientRpc(ClientRpcParams clientRpcParams)
+  {
+    Debug.Log(OwnerClientId + " :: TestClientRpc :: ");
   }
 }
